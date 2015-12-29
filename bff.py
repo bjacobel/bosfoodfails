71592c7a0869f7ca565aace053b1e3a2a1b8b034
Brian Jacobel <bjacobel@gmail.com>
2015-12-28 21:35:12 -0600

    Connected to Twitter

--- bff.py
+++ bff.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from addict import Dict
+from datetime import datetime
 from sodapy import Socrata
 import boto3
 import botocore.session
-from datetime import datetime
 import os
 import re
+import tweepy
+
+config = Dict()
 
 
-def get_config(context):
-    if not context:  # Running in development
-        session = botocore.session.get_session()
-        session.profile = 'bjacobel'
-        boto3.setup_default_session(botocore_session=session)
+def get_config():
+    session = botocore.session.get_session()
+    session.profile = 'bjacobel'
+    boto3.setup_default_session(botocore_session=session)
 
     kms = boto3.client('kms')
-
-    config = Dict()
 
     for secret in os.listdir('./secrets'):
         with open('./secrets/' + secret, 'rb') as f:
@@ -25,7 +25,46 @@ def get_config(context):
                 CiphertextBlob=f.read()
             )['Plaintext']
 
-    return config
+
+def save_dynamo(text):
+    """Save this <140 char sequence to Dynamo"""
+    dynamo = boto3.client('dynamodb')
+
+    dynamo.put_item(
+        TableName='BosFoodFails',
+        Item={
+            'text': {
+                'S': text
+            }
+        }
+    )
+
+
+def query_dynamo(text):
+    """return true if this text already exists in Dynamo"""
+    dynamo = boto3.client('dynamodb')
+
+    resp = dynamo.get_item(
+        TableName='BosFoodFails',
+        Key={
+            'text': {
+                'S': text
+            }
+        }
+    )
+
+    if 'Item' in resp and resp['Item']['text']['S'] == text:
+        return True
+    return False
+
+
+def tweet(text):
+    auth = tweepy.OAuthHandler(config.TwitterConsumerKey, config.TwitterConsumerSecret)
+    auth.set_access_token(config.TwitterAccessToken, config.TwitterAccessTokenSecret)
+
+    api = tweepy.API(auth)
+
+    api.update_status(text)
 
 
 def correct_case(str):
@@ -51,11 +90,16 @@ def handle_violation(viol):
     if len(text) > 140:
         text = text[:139] + u'…'
 
-    print(text)
+    if not query_dynamo(text):
+        print('Violation not found in Dynamo, saving it there and tweeting it')
+        save_dynamo(text)
+        tweet(text)
+    else:
+        print("Violation already known to Dynamo")
 
 
 def lambda_handler(event, context):
-    config = get_config(context)
+    get_config()
 
     client = Socrata(
         domain='data.cityofboston.gov',
@@ -64,9 +108,7 @@ def lambda_handler(event, context):
         password=config.SocrataPassword
     )
 
-    query = 'violstatus = \'Fail\''
-
-    viols = client.get('427a-3cn5', where=query, order='violdttm DESC', limit=100)
+    viols = client.get('427a-3cn5', where='violstatus = \'Fail\'', order='violdttm DESC', limit=20)
 
     client.close()
 
