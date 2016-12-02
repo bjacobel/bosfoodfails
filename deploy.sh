bc1bb1de5de5827d9b075615213076e3909a7aed
Brian Jacobel <bjacobel@gmail.com>
2016-12-02 11:10:39 -0500

    Deploy script was broken and not deploying dependencies. karma

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-rm bff.zip
+dir="$(pwd)"
+
 zip -ru bff.zip ./*.py
 zip -ru bff.zip ./secrets -x secrets/dev/*
 
+source ~/.virtualenvs/bosfoodfails/bin/activate
+pip install -r requirements.txt
+deactivate
+
 pushd ~/.virtualenvs/bosfoodfails/lib/python2.7/site-packages
-zip -ru $(pwd)/bff.zip ./*
+zip -ru $dir/bff.zip ./*
 popd
 
 aws lambda update-function-code \
   --profile=bjacobel \
   --function-name BosFoodFails \
-  --zip-file fileb://$(pwd)/bff.zip
+  --zip-file fileb://$dir/bff.zip
