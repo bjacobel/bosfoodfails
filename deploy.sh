d2c5a1df3a0626f9a18767786c7b7b7f6b72edae
Brian Jacobel <bjacobel@gmail.com>
2016-12-02 09:35:15 -0500

    Moved folders, make this just use pwd

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ zip -ru bff.zip ./*.py
 zip -ru bff.zip ./secrets -x secrets/dev/*
 
 pushd ~/.virtualenvs/bosfoodfails/lib/python2.7/site-packages
-zip -ru ~/code/bosfoodfails/bff.zip ./*
+zip -ru $(pwd)/bff.zip ./*
 popd
 
 aws lambda update-function-code \
   --profile=bjacobel \
   --function-name BosFoodFails \
-  --zip-file fileb://~/code/bosfoodfails/bff.zip
+  --zip-file fileb://$(pwd)/bff.zip
